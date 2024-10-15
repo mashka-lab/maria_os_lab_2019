d0844e7c94e44e383615f6cd0cc7f8df20e58a88
mashka-lab <kammashabor@mail.ru>
2024-10-15 16:56:07 +0300

    Update revert_string.c

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,19 +1,19 @@
-#include "revert_string.h"
-#include <string.h>
+#include "revert_string.h" //заголовочный файл 
+#include <string.h> //библиотека для работы со строками
 #include <stdlib.h>
 
-void RevertString(char *str)
+void RevertString(char *str) //создю ф-цию которая принимает строку char *str которую нужно развернуть 
 {
-	int str_len = strlen(str);
-	char* newStr = malloc(sizeof(char)*str_len);
+	int str_len = strlen(str); //получаю измеряю длину строки  
+	char* newStr = malloc(sizeof(char)*str_len); //выделяю память под новую строку 
 
-	for (int i = str_len - 1; i > -1; i--)
+	for (int i = str_len - 1; i > -1; i--) //создаю цикл для заполнения новой строки символами в обратном порядке 
 	{
-		newStr[str_len - 1 - i] = str[i];
+		newStr[str_len - 1 - i] = str[i]; //цикл проходит по строке str с конца индекс i идет от str_len - 1 до 0 и копирует каждый символ в строку newStr начиная с первого индекса 
 	}
-	for (int i = 0; i < str_len; i++)
+	for (int i = 0; i < str_len; i++) //копирование символов из новой строки обратно в исходную строку 
 	{
 		str[i] = newStr[i];
 	}
-	free(newStr);
+	free(newStr); //освобождаю память 
 }
